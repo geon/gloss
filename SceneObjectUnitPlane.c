26baaed2410eebc4f3b7076006d1a959d2912a50
Victor Widell <victor@topmost.se>
2013-09-01 02:10:59 +0200

    Implemented photon emission in SceneObjectUnitPlane.

--- SceneObjectUnitPlane.c
+++ SceneObjectUnitPlane.c
@@ -44,9 +44,49 @@ Intersection sceneObjectUnitPlaneIntersectRay(const SceneObject *superobject, co
 
 bool sceneObjectUnitPlaneEmitPhotons(const SceneObject *superobject, const int numPhotons, PhotonContainer *photons) {
 
-	// TODO: Implement this. Use code from SceneObjectSphere.
+	const SceneObjectUnitPlane *object = (SceneObjectUnitPlane *) superobject;
+	
+	Color flux = sceneObjectUnitPlaneRadiantFlux(superobject);
+	
+	// See comments in SceneObjectSphere.
 
-	return false;
+	// The U and V axis in the plane.
+	Vector n = object->plane.normal;
+	Vector U = makeVector(n.y, n.z, n.x);
+	Vector V = makeVector(n.z, n.x, n.y);
+	
+	// Divide the photons onto a grid of n*m, most closely matching the wanted number.
+	int numPhotonsU = ceil(sqrt(numPhotons/2.0))*2;
+	int numPhotonsV = numPhotons/numPhotonsU;
+
+	// How many photons in the last row, and how tall it is.
+	int lastRowNumPhotonsU = numPhotons - numPhotonsU*numPhotonsV;
+	float lastRowFactor = lastRowNumPhotonsU / (float) numPhotons;
+	
+	for (int iV = 0; iV < numPhotonsV; ++iV) {
+		
+		for (int iU = 0; iU < numPhotonsU; ++iU) {
+			
+			float u = (iU + randf()) / numPhotonsU;
+			float v = (iV + randf()) / numPhotonsV * (1-lastRowFactor);
+			
+			Vector position = vAdd(vsMul(U, u*2-1), vsMul(V, v*2-1));
+			
+			photonContainerAddValue(photons, makePhoton(makeRay(position, vSampleHemisphere(object->plane.normal)), csMul(flux, 1.0 / numPhotons)));
+		}
+	}
+	
+	for (int iU = 0; iU < lastRowNumPhotonsU; ++iU) {
+		
+		float u = (iU + randf()) / lastRowNumPhotonsU;
+		float v = (1 - lastRowFactor) + randf() * lastRowFactor;
+		
+		Vector position = vAdd(vsMul(U, u*2-1), vsMul(V, v*2-1));
+		
+		photonContainerAddValue(photons, makePhoton(makeRay(position, vSampleHemisphere(object->plane.normal)), csMul(flux, 1.0 / numPhotons)));
+	}
+	
+	return true;
 }
 
 Color sceneObjectUnitPlaneRadiantFlux(const SceneObject *superobject) {
